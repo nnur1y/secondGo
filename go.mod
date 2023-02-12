458a0eac28702dff07b3ed0409e12a83f243d26a
nnur1y <nurly125@mail.ru>
2023-02-12 23:54:38 +0600

    new commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/nnur1y/secondGo
+
+go 1.20
+
+require github.com/nnur1y/firstGo v0.0.0-20230212174652-a86c1cd6e26e
---
+++ secondGo.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"github.com/nnur1y/firstGo"
+)
+
+func main() {
+	firstGo.UserInfo()
+	firstGo.RecipeInfo()
+	firstGo.AuthorCheck()
+	firstGo.CalorieCheck()
+}
